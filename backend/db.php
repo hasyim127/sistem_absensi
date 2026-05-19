d81fcb342b3e0fa66b2c448d457a75b95ae8852a
hasyim172 <hasyimasyarimail@gmail.com>
2026-05-19 12:04:08 +0700

    feat: API login dan koneksi DB

---
+++ backend/db.php
@@ -0,0 +1,8 @@
+<?php
+  include 'config.php';
+  $conn = mysqli_connect($host, $user, $pass, $db);
+
+  if (!$conn) {
+      die("Koneksi gagal: " . mysqli_connect_error());
+  }
+?>
---
+++ backend/login.php
@@ -0,0 +1,16 @@
+<?php
+  include 'db.php';
+
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  $query = mysqli_query($conn, "SELECT * FROM users WHERE username='$username'");
+
+  $user = mysqli_fetch_assoc($query);
+
+  if ($user && $user['password'] == $password) {
+      echo json_encode(["status" => "success"]);
+  } else {
+      echo json_encode(["status" => "error"]);
+  }
+?>
